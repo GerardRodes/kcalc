a670a9f7ca32d09c67952d3e1fb9efa879e91869
Gerard Rodes <gerard.rodes@hungerstation.com>
2024-02-11 14:23:09 +0400

    x

--- internal/server/request.go
+++ internal/server/request.go
@@ -1,10 +1,14 @@
 package server
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"reflect"
+	"strconv"
 	"strings"
 
+	"github.com/GerardRodes/kcalc/internal"
 	"github.com/davecgh/go-spew/spew"
 )
 
@@ -20,20 +24,49 @@ func parseReqMultipart[T any](r *http.Request) (T, error) {
 	rt := reflect.TypeOf(zero)
 	spew.Dump(rt)
 
-	type fieldt struct {
-		parser func(string) any
-	}
-	fields := make(map[string]fieldt, rt.NumField())
+	// todo: cache this
+	fields := make(map[string]func(string) (any, error), rt.NumField())
 
 	for i := 0; i < rt.NumField(); i++ {
 		rf := rt.Field(i)
 		spew.Dump(rf)
+
 		switch rf.Type.Kind() {
+		case reflect.String:
+			fields[strings.ToLower(rf.Name)] = func(val string) (any, error) {
+				return val, nil
+			}
 		case reflect.Int64:
-			fields[strings.ToLower(rf.Name)] = fieldt{}
+			fields[strings.ToLower(rf.Name)] = func(val string) (any, error) {
+				out, err := strconv.ParseInt(val, 10, 64)
+				if err != nil {
+					return nil, internal.NewSErr(errors.New("invalid kcal format"), err)
+				}
+
+				return out, nil
+			}
+		case reflect.Float64:
+			fields[strings.ToLower(rf.Name)] = func(val string) (any, error) {
+				out, err := strconv.ParseFloat(val, 64)
+				if err != nil {
+					return nil, internal.NewSErr(errors.New("invalid kcal format"), err)
+				}
+
+				return out, nil
+			}
+		case reflect.Struct:
+			switch rf.Type.PkgPath() {
+			case "github.com/GerardRodes/kcalc/internal.Image":
+				switch rf.Type.Name() {
+				case "Image":
+					fields[strings.ToLower(rf.Name)] = func(name string) (any, error) {
+						spew.Dump("internal.Image", name)
+						return nil, nil
+					}
+				}
+			}
 		default:
-			spew.Dump(rt)
-			panic("unsupported field kind")
+			panic(fmt.Sprintf("unsupported field kind %q", rf.Type.Kind()))
 		}
 	}
 
